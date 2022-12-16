b0f280b4cbf79615046a0e382202743d2d765416
Asherlr <73479844+asherlr23@users.noreply.github.com>
2022-12-16 04:36:32 +0000

    New RenderManager function (i forgot to stage the changes before commiting

--- classes/RenderManager.cs
+++ classes/RenderManager.cs
@@ -2,15 +2,16 @@ using System;
 
 class renderManager 
 {
-    public void RenderObject(params int[] vertexes, int writeType)
+    private char[] characterSet = {"|", ".", "-","*","#"};
+    public void RenderObjectBar(int length, int writeType = 1)
     {
-        for(int i = 0; i > vertexes.Length * 2; i++) 
-        {
-            for 
-           if(writeType == 1)
-           {
+            string current;
 
-           }
-        }
+            for(int i = 0; i > length; i++)
+            {
+               current = current + characterSet[writeType];
+               Console.Clear();
+               Console.WriteLine(current);
+            }    
     }
 }
